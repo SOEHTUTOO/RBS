754e00af3c3ce60e5fa29465750a88dbf8fbfaba
soehtutoo <soehtutoo@SOE-PC>
2018-04-11 01:33:51 +0630

    RBS v0.3.3b

--- src/room/booking/system/controller/AddMemberController.java
+++ src/room/booking/system/controller/AddMemberController.java
@@ -6,11 +6,18 @@
 package room.booking.system.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.sql.Date;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +29,26 @@ public class AddMemberController implements Initializable {
 
     @FXML
     private JFXButton cancelBtn;
+    @FXML
+    private JFXTextField nameField;
+    @FXML
+    private JFXTextField idField;
+    @FXML
+    private JFXTextField arcField;
+    @FXML
+    private DatePicker birthDatePicker;
+    @FXML
+    private JFXCheckBox maleCheck;
+    @FXML
+    private JFXCheckBox femaleCheck;
+    @FXML
+    private JFXTextField mobileField;
+    @FXML
+    private JFXTextField emailField;
+    @FXML
+    private JFXTextArea addressField;
+    @FXML
+    private JFXButton saveBtn;
 
     /**
      * Initializes the controller class.
@@ -36,6 +63,40 @@ public class AddMemberController implements Initializable {
         
         Stage stage = (Stage) cancelBtn.getScene().getWindow();
         stage.close();
+        
+    }
+
+    @FXML
+    private void saveMember(ActionEvent event) {
+        
+        String name = nameField.getText();
+        String id = idField.getText();
+        String arcNo = arcField.getText();
+        Date birth = java.sql.Date.valueOf(birthDatePicker.getValue());
+        String gender = null;
+        
+        if(maleCheck.isSelected() && femaleCheck.isSelected()) {
+        
+           gender = "Please just choose one gender";
+            
+        }else if(maleCheck.isSelected()){
+        
+           gender = "Male";
+            
+        }else if(femaleCheck.isSelected()){
+        
+           gender = "Female";
+            
+        }else{
+        
+           gender = "Please choose your gender";
+        }
+
+        String mobile = mobileField.getText();
+        String email = emailField.getText();
+        String address = addressField.getText();
+        
+        System.out.println(name+id+arcNo+birth+gender+mobile+email+address);
         
     }
     
