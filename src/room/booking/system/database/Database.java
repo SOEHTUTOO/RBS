2239e5e47f40ee1cd3de83a9f2f6a0032c9b0c6a
soehtutoo <soehtutoo@SOE-PC>
2018-04-10 20:39:00 +0630

    RBS v0.3.1

---
+++ src/room/booking/system/database/Database.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package room.booking.system.database;
+
+/**
+ *
+ * @author soehtutoo
+ */
+public class Database {
+    
+    public void connect() {
+    
+        
+        
+    }
+    
+}
